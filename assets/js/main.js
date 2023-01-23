2b4970af8b490c05b535b58cd469e3ea430f8d03
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-23 21:12:15 +0100

    buttonStyle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
 console.log("klappts?")
+
+/* 
+Frage
+
+4 SuperBalls erstellen
+
+Farbe body + h1 ändern
+
+Elemente in css stylen 
+
+
+wenn click auf Ball     dann body + h1 HintergrundFarbe ändern
+
+
+*/
